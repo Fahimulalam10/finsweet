65cbdfb84ef368a7af65d84f738e08ce8696c87b
Fahimulalam10 <capfahim2323@gmail.com>
2023-06-12 01:08:51 +0600

    create client area

---
+++ script.js
@@ -0,0 +1,13 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+      
+    }
+})
